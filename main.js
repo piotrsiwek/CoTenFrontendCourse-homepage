81e80400b31ac265916e641e233c951343b3bed5
piotrsiwek <59854137+piotrsiwek@users.noreply.github.com>
2024-02-08 01:11:14 +0100

    styling

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const firstName = 'Piotr Siwek';
 let age = 42;
-
-
-
 console.log(firstName);
 console.log(age);
 
@@ -10,3 +7,6 @@ console.log(age);
 const header = document.querySelector('.page-header__heading--js');
 
 //header.innerHTML = `My name is ${firstName} and my age is ${age}`;
+
+const contenJS = document.querySelector('.my-article__content--js');
+contenJS.innerHTML = `wypełniam treść artykułu z JS`;
