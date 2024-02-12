f8dff1fd852925309989b7509e1ce11d4843b4df
piotrsiwek <59854137+piotrsiwek@users.noreply.github.com>
2024-02-12 22:14:21 +0100

    Update main.js with new content and functions

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
-const firstName = 'Piotr Siwek';
-let age = 42;
-console.log(firstName);
-console.log(age);
-
-
 const header = document.querySelector('.page-header__heading--js');
 
 //header.innerHTML = `My name is ${firstName} and my age is ${age}`;
 
 const contenJS = document.querySelector('.my-article__content--js');
-contenJS.innerHTML = `wypełniam treść artykułu z JS wypełniam treść artykułu z JS wypełniam treść artykułu z JS`;
+contenJS.innerHTML = `wypełniam treść artykułu z JS wypełniam treść artykułu z JS wypełniam treść artykułu z JSr `;
+
+/* const calculate = (myNumber) => {
+    myNumber = myNumber +3 ;
+    console.log(myNumber);
+    return myNumber*23;
+
+} */
+
+const calculate2 = (myNumber) => (myNumber+2)*2;
+
+
+/* calculate(12);
+ */
+console.log(calculate2(13));
+
+const deathStar = {
+
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} desstssstroyed`);
+    },
+    
+    isOperating: true,
+    levels: 357,
+    name: 'Death Star',
+    population: 10000,
+    isLightOn: true,
+    commander: {
+        name: 'Darth Vader',
+        age: 44,
+    }
+}
+
+console.log(deathStar.commander.name);
+
+deathStar.fire('Rebel ship');
